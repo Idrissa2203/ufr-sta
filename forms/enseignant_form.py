6f1c0c4de7ff2a5277079f576e553cbe45b85d9d
faayb <barry.fatoumata@uam.edu.sn>
2026-06-27 01:53:43 +0000

    feat: ajout des formulaires pour Departements,Formations, Enseignants

---
+++ forms/enseignant_form.py
@@ -0,0 +1,12 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, TextAreaField, SelectField, FileField, SubmitField
+from wtforms.validators import DataRequired, Email
+
+class EnseignantForm(FlaskForm):
+departement_id = SelectField('Département', coerce=int, validators=[DataRequired()])
+nom = StringField('Nom complet', validators=[DataRequired()])
+grade = StringField('Grade')
+email = StringField('Email professionnel', validators=[Email()])
+domaines_recherche = TextAreaField('Domaines de recherche')
+photo = FileField('Photo (fichier image)')
+submit = SubmitField('Enregistrer')
